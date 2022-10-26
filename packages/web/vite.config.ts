05d2cdf6d7682809cb62ba792196fd755e9bb4d1
pchmn <pchmn.dev@gmail.com>
2022-10-26 19:43:29 +0200

    build: init pwa

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,10 +1,39 @@
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Prevezic',
+        short_name: 'Prevezic',
+        description: 'My Awesome App description',
+        theme_color: '#FFE8CC',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     sourcemap: true
   },
