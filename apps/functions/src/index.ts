b8fab7db741d14c00c86520fd19843a04641330e
pchmn <pchmn.dev@gmail.com>
2022-11-28 19:10:34 +0100

    refactor(functions): rename function

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -1,8 +1,8 @@
 import { user } from 'firebase-functions/v1/auth';
 import { onCall } from 'firebase-functions/v1/https';
 
-export const signInWithEmailLink = onCall(async (data, context) => {
-  await (await import('./signInWithEmailLink')).default(data, context);
+export const sendSignInWithEmailLink = onCall(async (data, context) => {
+  await (await import('./sendSignInWithEmailLink')).default(data, context);
 });
 
 export const processSignUp = user().onCreate(async (user) => {
