aeccc5a1aa249b394230f304a5567f545054785b
pchmn <pchmn.dev@gmail.com>
2022-12-04 20:46:16 +0100

    build: init firebase emulators

--- apps/web/src/conf/firebase.ts
+++ apps/web/src/conf/firebase.ts
@@ -1,4 +1,7 @@
 import { initializeApp } from 'firebase/app';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,4 +12,15 @@ const firebaseConfig = {
   appId: '1:756699426455:web:dff09e29d749583ca903d2',
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+if (import.meta.env.DEV) {
+  const auth = getAuth();
+  connectAuthEmulator(auth, 'http://192.168.1.10:9099');
+
+  const db = getFirestore();
+  connectFirestoreEmulator(db, '192.168.1.10', 8080);
+
+  const functions = getFunctions(app);
+  connectFunctionsEmulator(functions, '192.168.1.10', 5001);
+}
