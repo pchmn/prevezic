1e15317a7593a68a0a9685dbac8f370eeeb657d4
pchmn <pchmn.dev@gmail.com>
2022-12-07 00:01:54 +0100

    fix(web): i18n

--- apps/web/src/app/modules/signIn/components/SignIn.tsx
+++ apps/web/src/app/modules/signIn/components/SignIn.tsx
@@ -1,15 +1,13 @@
 import { Button, Flex, Input, Space, Text, Title } from '@mantine/core';
 import { useInputState, useLocalStorage } from '@mantine/hooks';
 import { useFirebaseAuth } from '@prevezic/react';
-import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
 export function SignIn() {
   const [emailValue, setEmailValue] = useInputState('');
   const [, setEmail] = useLocalStorage({ key: 'emailForSignIn' });
 
   const { sendMagicLink, loading } = useFirebaseAuth();
-
-  const { t } = useTranslation();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -21,14 +19,14 @@ export function SignIn() {
 
   return (
     <Flex h="100%" align="center" direction="column" gap="xl">
-      <Title order={2}>{t('signIn.title')}</Title>
-      <Text>{t('signIn.description')}</Text>
+      <Title order={2}>{i18next.t('signIn.title')}</Title>
+      <Text>{i18next.t('signIn.description')}</Text>
       <Space h={30} />
       <form onSubmit={handleSubmit}>
         <Flex direction="column" gap="md" w={{ xs: '100%', sm: 300 }}>
-          <Input placeholder={t('signIn.emailPlaceholder')} value={emailValue} onChange={setEmailValue} />
+          <Input placeholder={i18next.t('signIn.emailPlaceholder')} value={emailValue} onChange={setEmailValue} />
           <Button type="submit" disabled={!emailValue || loading} loading={loading}>
-            {t('signIn.sendMagicLink')}
+            {i18next.t('signIn.sendMagicLink')}
           </Button>
         </Flex>
       </form>
