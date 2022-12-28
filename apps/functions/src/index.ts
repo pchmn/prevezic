ffb546cfedd10b2514fca59dbc364c66bbbb24ac
pchmn <pchmn.dev@gmail.com>
2022-12-29 00:58:31 +0100

    feat(functions): change functions region

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -1,17 +1,18 @@
 import { FunctionName } from '@prevezic/core';
-import { document } from 'firebase-functions/v1/firestore';
-import { onCall } from 'firebase-functions/v1/https';
+import { region } from 'firebase-functions/v1';
 
 const functions: Record<FunctionName, unknown> = {
-  sendMagicLink: onCall(async (data, context) => {
+  sendMagicLink: region('europe-west1').https.onCall(async (data, context) => {
     await (await import('./sendMagicLink')).default(data, context);
   }),
-  mergeUsers: onCall(async (data, context) => {
+  mergeUsers: region('europe-west1').https.onCall(async (data, context) => {
     await (await import('./mergeUsers')).default(data, context);
   }),
-  processAlbumCreation: document('albums/{albumId}').onCreate(async (snapshot, context) => {
-    await (await import('./processAlbumCreation')).default(snapshot, context);
-  }),
+  processAlbumCreation: region('europe-west1')
+    .firestore.document('albums/{albumId}')
+    .onCreate(async (snapshot, context) => {
+      await (await import('./processAlbumCreation')).default(snapshot, context);
+    }),
 };
 
 const { sendMagicLink, mergeUsers, processAlbumCreation } = functions;
--- packages/react/src/core/firebase/functions/index.ts
+++ packages/react/src/core/firebase/functions/index.ts
@@ -2,5 +2,5 @@ import { CallableFunctionName, FunctionParams } from '@prevezic/core';
 import { getFunctions, httpsCallable } from 'firebase/functions';
 
 export function callFunction<P extends CallableFunctionName, R>(functionName: P, params: FunctionParams[P]) {
-  return httpsCallable<FunctionParams[P], R>(getFunctions(), functionName)(params);
+  return httpsCallable<FunctionParams[P], R>(getFunctions(undefined, 'europe-west1'), functionName)(params);
 }
