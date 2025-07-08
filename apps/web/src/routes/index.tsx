063d64fdc2127a0620c5aea3d6312c2840f95906
pchmn <pchmn.dev@gmail.com>
2025-07-08 22:36:01 +0200

    feat(web): handle enter joining code

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { convexQuery } from '@convex-dev/react-query';
 import { api } from '@prevezic/backend/_generated/api';
+import { Button } from '@prevezic/ui/button';
 import { Flex } from '@prevezic/ui/flex';
+import { Input } from '@prevezic/ui/input';
 import { Spinner } from '@prevezic/ui/spinner';
 import { useQuery } from '@tanstack/react-query';
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import '../App.css';
 
 export const Route = createFileRoute('/')({
@@ -16,17 +18,42 @@ function App() {
 
   const { data: projects, isPending } = useQuery({
     ...convexQuery(api.project.list, {}),
+    networkMode: 'online',
   });
+
+  const [code, setCode] = useState('');
 
   useEffect(() => {
     console.log({ isPending, projects });
-    if (!isPending && projects && projects.length > 0) {
+    if (isPending) {
+      return;
+    }
+    if (projects && projects.length > 0) {
       navigate({
         to: '/projects/$projectId',
         params: { projectId: projects[0]._id },
       });
     }
   }, [isPending, projects, navigate]);
+
+  if (!isPending && projects?.length === 0) {
+    return (
+      <Flex align='center' justify='center' className='h-screen' gap='md'>
+        <Input
+          placeholder='Entrer le code...'
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <Button
+          onClick={() =>
+            navigate({ to: '/projects/join', search: { token: code } })
+          }
+        >
+          Rejoindre
+        </Button>
+      </Flex>
+    );
+  }
 
   return (
     <Flex align='center' justify='center' className='h-screen'>
