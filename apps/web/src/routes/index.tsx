c2891d29b45602b686874d67bb192016df12ee72
pchmn <pchmn.dev@gmail.com>
2025-07-08 17:37:00 +0200

    feat(web): loading state when fetching projects

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,10 +1,10 @@
 import { convexQuery } from '@convex-dev/react-query';
 import { api } from '@prevezic/backend/_generated/api';
-import { Button } from '@prevezic/ui/button';
+import { Flex } from '@prevezic/ui/flex';
+import { Spinner } from '@prevezic/ui/spinner';
 import { useQuery } from '@tanstack/react-query';
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
-import { appConfig } from '~/config/config';
-import { authClient } from '~/lib/auth.client';
+import { useEffect } from 'react';
 import '../App.css';
 
 export const Route = createFileRoute('/')({
@@ -14,54 +14,23 @@ export const Route = createFileRoute('/')({
 function App() {
   const navigate = useNavigate();
 
-  const {
-    data: projects,
-    isPending,
-    isFetching,
-    isLoading,
-  } = useQuery({
+  const { data: projects, isPending } = useQuery({
     ...convexQuery(api.project.list, {}),
     initialData: [],
   });
 
-  console.log('isPending', { isPending, isFetching });
+  useEffect(() => {
+    if (!isPending && projects.length > 0) {
+      navigate({
+        to: '/projects/$projectId',
+        params: { projectId: projects[0]._id },
+      });
+    }
+  }, [isPending, projects, navigate]);
 
-  if (!isPending && projects?.length === 1) {
-    navigate({
-      to: '/projects/$projectId',
-      params: { projectId: projects[0]._id },
-    });
-  }
-
-  return isPending ? (
-    <div>Loading...</div>
-  ) : (
-    <div>
-      <Button onClick={addPhoto}>Click me</Button>
-      <input
-        type='file'
-        accept='image/*'
-        capture
-        onChange={(e) => {
-          console.log(e.target.files);
-        }}
-      />
-    </div>
+  return (
+    <Flex align='center' justify='center' className='h-screen'>
+      <Spinner className='w-8 h-8' />
+    </Flex>
   );
-}
-
-async function addPhoto() {
-  const sendImageUrl = new URL(`${appConfig.convexSiteUrl}/add-photo`);
-  sendImageUrl.searchParams.set('author', 'Jack Smith');
-
-  const { data } = await authClient.convex.token();
-
-  await fetch(sendImageUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'image/png',
-      Authorization: `Bearer ${data?.token}`,
-    },
-    body: 'test',
-  });
 }
